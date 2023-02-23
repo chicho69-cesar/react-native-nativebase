b1ad5cec16c303f49186f22ced1d98463e29d37a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-02-22 20:55:31 -0600

    :sparkles: Overlay Menu

--- src/overlay/Overlay.jsx
+++ src/overlay/Overlay.jsx
@@ -1,10 +1,16 @@
 import { NativeBaseProvider, Center } from "native-base";
 import { BasicAlertDialog } from "./MyAlertDialog";
+import { BasicMenu, GroupMenu, MenuOptionGroup, MenuPlacement } from "./MyMenu";
 
 export default MyOverlay = () => {
   return <NativeBaseProvider>
     <Center flex={1} px="3">
-      <BasicAlertDialog/>
+      {/* <BasicAlertDialog/> */}
+
+      {/* <BasicMenu/> */}
+      {/* <GroupMenu/> */}
+      {/* <MenuOptionGroup/> */}
+      <MenuPlacement/>
     </Center>
   </NativeBaseProvider>;
 }

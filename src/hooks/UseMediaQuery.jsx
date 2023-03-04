9cb6ff1f1de9cd9cf72f0fee4dd253d2f4056ff3
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-04 14:54:26 -0600

    :sparkles: Hooks useMediaQuery

---
+++ src/hooks/UseMediaQuery.jsx
@@ -0,0 +1,140 @@
+import { useMediaQuery, HStack, VStack, Center, Skeleton } from 'native-base';
+
+export const MaxHeight = () => {
+  const [ isSmallScreen ] = useMediaQuery({
+    minHeight: 280,
+    maxHeight: 480
+  });
+
+  return <Center w="100%">
+    {isSmallScreen 
+      ? <HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack> 
+
+      : <HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <Skeleton flex="1" h="150" rounded="md" />
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack>
+    }
+  </Center>;
+}
+
+export const MinWidth = () => {
+  const [ isSmallScreen ] = useMediaQuery({
+    minWidth: 280
+  });
+
+  return <Center w="100%">
+    {isSmallScreen 
+      ? <HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack> 
+      
+      : <HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <Skeleton flex="1" h="150" rounded="md" />
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack>
+    }
+  </Center>;
+}
+
+export const Orientation = () => {
+  const [ isLandScape, isPortrait ] = useMediaQuery([
+    { orientation: "landscape" }, 
+    { orientation: "portrait" }
+  ]);
+
+  return <Center w="100%">
+    {isPortrait 
+      &&<HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack>
+    }
+
+    {isLandScape 
+      &&<HStack 
+          w="90%" maxW="400" borderWidth="1" 
+          space={8} rounded="md" 
+          borderColor="coolGray.400" p="4"
+        >
+        <Skeleton flex="1" h="150" rounded="md" />
+        <VStack flex="3" space="4">
+          <Skeleton />
+          <Skeleton.Text />
+
+          <HStack space="2" alignItems="center">
+            <Skeleton size="5" rounded="full" />
+            <Skeleton h="3" flex="2" rounded="full" />
+            <Skeleton h="3" flex="1" rounded="full" />
+          </HStack>
+        </VStack>
+      </HStack>
+    }
+  </Center>;
+}

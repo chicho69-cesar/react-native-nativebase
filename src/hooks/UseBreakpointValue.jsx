2093a3a71026c55fc9bd3d8a3116409c4c3339fc
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-04 14:53:51 -0600

    :sparkles: Hooks UseBreakpointValue

---
+++ src/hooks/UseBreakpointValue.jsx
@@ -0,0 +1,77 @@
+import { useBreakpointValue, Icon, Text, VStack, Heading, ScrollView } from 'native-base';
+
+export const UseBreakpointValue = () => {
+  const flexDir = useBreakpointValue({
+    base: "column",
+    lg: "row"
+  });
+
+  return <ScrollView showsVerticalScrollIndicator={false}>
+    <VStack 
+      py="8" space={8} 
+      alignItems="center" 
+      justifyContent="center"
+    >
+      <Heading>Why us?</Heading>
+      <View style={{ flexDirection: flexDir }}>
+        <VStack 
+          m="3" w="140" borderRadius="xl" 
+          p="3" bg="cyan.200" space={2} 
+          alignItems="center" 
+          justifyContent="center"
+        >
+          <Icon 
+            as={Foundation} name="shield" 
+            size="sm" textAlign="center" 
+            _dark={{ color: "coolGray.800" }} 
+          />
+
+          <Text 
+            fontSize="lg" textAlign="center" 
+            _dark={{ color: "coolGray.800" }}
+          >
+            Secure Checkout
+          </Text>
+        </VStack>
+
+        <VStack 
+          m="3" w="140" borderRadius="xl" 
+          p="3" bg="cyan.200" space={2} 
+          alignItems="center" justifyContent="center"
+        >
+          <Icon 
+            as={Foundation} name="shield" 
+            size="sm" textAlign="center" 
+            _dark={{ color: "coolGray.800" }} 
+          />
+
+          <Text 
+            fontSize="lg" textAlign="center" 
+            _dark={{ color: "coolGray.800" }}
+          >
+            Secure Checkout
+          </Text>
+        </VStack>
+
+        <VStack 
+          m="3" w="140" borderRadius="xl" 
+          p="3" bg="cyan.200" space={2} 
+          alignItems="center" justifyContent="center"
+        >
+          <Icon 
+            as={Feather} name="clock" 
+            size="sm" textAlign="center" 
+            _dark={{ color: "coolGray.800" }} 
+          />
+
+          <Text 
+            fontSize="lg" textAlign="center" 
+            _dark={{ color: "coolGray.800" }}
+          >
+            Fast Turn Around
+          </Text>
+        </VStack>
+      </View>
+    </VStack>
+  </ScrollView>;
+}

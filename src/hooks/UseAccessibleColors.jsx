babfc22fa70eac856e45197d67883ae26fd38501
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-04 15:21:37 -0600

    :sparkles: Hooks UseAccesibleColors

---
+++ src/hooks/UseAccessibleColors.jsx
@@ -0,0 +1,56 @@
+import {
+  Center, 
+  Button, 
+  Text, 
+  Stack,
+  HStack,
+  Switch, 
+  useAccessibleColors, 
+  useTheme,
+} from "native-base";
+import { useState } from "react";
+
+const ButtonTemplate = ({ shade }) => {
+  const colorContrast = useContrastText(`emerald.${shade}`);
+  
+  return <Center>
+    <Button 
+      colorScheme="emerald" 
+      key={`emerald.${shade}`} 
+      bg={`emerald.${shade}`} 
+      _text={{ color: colorContrast }} 
+      mb={1}
+    >
+      Save
+    </Button>
+  </Center>;
+};
+
+export const UseAccessibleColors = () => {
+  const [ value, setValue ] = useState(false);
+  const [,, toggleAccessibleColors] = useAccessibleColors();
+  const {
+    colors
+  } = useTheme();
+
+  return <Center>
+    <Stack space="3" direction={[ "column", "row" ]}>
+      {Object.keys(colors.yellow).map((key, index) => {
+        if (index > 2 && index < 9) return <ButtonTemplate shade={key} />;
+      })}
+    </Stack>
+
+    <HStack mt="6" space="3">
+      <Text>Toggle Accessible Colors</Text>
+
+      <Switch 
+        onValueChange={() => {
+          toggleAccessibleColors();
+          setValue(!value);
+        }} 
+        value={value}
+        colorScheme="coolGray" 
+      />
+    </HStack>
+  </Center>;
+}

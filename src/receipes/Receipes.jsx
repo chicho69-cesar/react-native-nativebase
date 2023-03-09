34885f554b822f330ab811b9bc1d8062c73f5922
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-08 21:07:50 -0600

    :art: Receipes AppDrawer

--- src/receipes/Receipes.jsx
+++ src/receipes/Receipes.jsx
@@ -1,12 +1,14 @@
 import { NativeBaseProvider, Center } from "native-base";
 import RDesigns from "./designs/RDesigns";
 import RForms from "./forms/RForms";
+import RLayout from "./layout/RLayout";
 
 export default Receipes = () => {
   return <NativeBaseProvider>
     <Center flex={1} px="3">
       {/* <RDesigns/> */}
-      <RForms/>
+      {/* <RForms/> */}
+      <RLayout/>
     </Center>
   </NativeBaseProvider>;
 }
---
+++ src/receipes/layout/RLayout.jsx
@@ -0,0 +1,8 @@
+import { View } from "native-base";
+import { AppDrawer } from "./AppDrawer";
+
+export default function RLayout() {
+  return <View w='100%'>
+    <AppDrawer/>
+  </View>;
+}

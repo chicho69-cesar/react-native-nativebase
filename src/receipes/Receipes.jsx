45ea069850df43d9ce3457c100b8b111235deb89
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-08 20:54:40 -0600

    :art: Receipes login/signup forms

--- src/receipes/Receipes.jsx
+++ src/receipes/Receipes.jsx
@@ -1,10 +1,12 @@
 import { NativeBaseProvider, Center } from "native-base";
 import RDesigns from "./designs/RDesigns";
+import RForms from "./forms/RForms";
 
 export default Receipes = () => {
   return <NativeBaseProvider>
     <Center flex={1} px="3">
-      <RDesigns/>
+      {/* <RDesigns/> */}
+      <RForms/>
     </Center>
   </NativeBaseProvider>;
 }
---
+++ src/receipes/forms/RForms.jsx
@@ -0,0 +1,9 @@
+import { View } from "native-base";
+import { LoginForm, SignUpForm } from "./ActionForms";
+
+export default function RForms() {
+  return <View w='100%'>
+    {/* <LoginForm/> */}
+    <SignUpForm/>
+  </View>;
+}
